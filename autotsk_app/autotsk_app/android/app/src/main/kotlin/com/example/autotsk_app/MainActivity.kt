a5758061397254afd3376db64e55ea9c4a864875
tayzhongkai@gmail.com <tayzhongkai@gmail.com>
2023-06-23 16:28:27 +0800

    updated UI for onboarding

--- autotsk_app/autotsk_app/android/app/src/main/kotlin/com/example/autotsk_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.autoTsk.autotsk_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- autotsk_app/autotsk_app/lib/utilities/dimension.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-// this is the dimension to determine when to switch to web mode.
-const webScreenSize = 600;
